55df50f46363ea27afe7170ba17568056b260454
bioflower <40282101+bioflower@users.noreply.github.com>
2026-07-12 20:50:01 -0400

    Add: longest-common-prefix - submission-5

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        shortest_str: str = strs[0]
+        
+        for i in reversed(range(len(shortest_str))):
+            prefix: str = shortest_str[:i+1]
+            contain_prefix: bool = True
+            for index, input_str in enumerate(strs):
+                if prefix not in input_str:
+                    contain_prefix = False
+                    break
+                if index == len(strs) - 1 and contain_prefix:
+                    return prefix
+        return ""
